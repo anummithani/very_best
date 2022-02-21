5f68616889a88f553a3962cf3650a8f1b85cbdb9
anonymous anonymous <anum.mithani@kellogg.northwestern.edu>
2022-02-21 17:50:50 +0000

    Generate graphiti resources for bookmarks

---
+++ app/resources/bookmark_resource.rb
@@ -0,0 +1,11 @@
+class BookmarkResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :venue_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
