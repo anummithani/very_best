1f8a36d8dd61e00710c5e7a5d79cb6dde932e476
anonymous anonymous <anum.mithani@kellogg.northwestern.edu>
2022-02-21 17:51:33 +0000

    Remove controller comments

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -4,25 +4,20 @@ class DishesController < ApplicationController
 
   before_action :set_dish, only: %i[show edit update destroy]
 
-  # GET /dishes
   def index
     @q = Dish.ransack(params[:q])
     @dishes = @q.result(distinct: true).includes(:user, :venue, :cuisine,
                                                  :bookmark).page(params[:page]).per(10)
   end
 
-  # GET /dishes/1
   def show; end
 
-  # GET /dishes/new
   def new
     @dish = Dish.new
   end
 
-  # GET /dishes/1/edit
   def edit; end
 
-  # POST /dishes
   def create
     @dish = Dish.new(dish_params)
 
@@ -38,7 +33,6 @@ class DishesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /dishes/1
   def update
     if @dish.update(dish_params)
       redirect_to @dish, notice: "Dish was successfully updated."
@@ -47,7 +41,6 @@ class DishesController < ApplicationController
     end
   end
 
-  # DELETE /dishes/1
   def destroy
     @dish.destroy
     message = "Dish was successfully deleted."
@@ -68,12 +61,10 @@ class DishesController < ApplicationController
     end
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_dish
     @dish = Dish.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def dish_params
     params.require(:dish).permit(:name, :cuisine_id, :venue_id, :loved,
                                  :user_id)
